4cebbbbe8658005746f6226b2475a9b7f4f9f8d4
huzaifa <connectalhuzaifiii@gmail.com>
2023-04-07 19:52:32 +0500

    Update main.js

--- JavaScript/Assignment-01/main.js
+++ JavaScript/Assignment-01/main.js
@@ -131,3 +131,15 @@ const students = [
 // Answer no 06
 // ---------------------------------------------------------------------------------------
 
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(i);
+    }
+  }
+  
